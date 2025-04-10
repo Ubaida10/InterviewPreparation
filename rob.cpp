44584224869f88d0bb070ba536c4e1604779444e
Muhammad Abu Ubaida Aljerah <123815303+Ubaida10@users.noreply.github.com>
2025-04-11 00:52:47 +0500

    LeetCode#198

---
+++ rob.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int rob(vector<int>& nums){
+    int n = nums.size();
+    if(n==0) return 0;
+    if(n==1) return nums[0];
+    if(n==2) return max(nums[0], nums[1]);
+    int dp[n+1];
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+    for(int i = 2; i < n; i++){
+        dp[i] = max(dp[i-1], (dp[i-2] + nums[i]));
+    }
+    return dp[n-1];
+}
+
+int main(){
+    vector<int> nums1 = {1, 2, 3, 1};
+    vector<int> nums2 = {2, 7, 9, 3, 1};
+    cout<<rob(nums1)<<endl;
+    cout<<rob(nums2)<<endl;
+    return 0;
+}
